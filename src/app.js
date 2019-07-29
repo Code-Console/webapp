743d638bf1647e33f00a380eb5e0e66257cab786
Shruti <Shrutibangar6@gmail.com>
2019-07-29 18:31:48 +0530

    ver : 1.1

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/weather', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'index.html'));
 // });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log('Your node js server is running');
 });
 const greeter = (name = 'user', age) => {
