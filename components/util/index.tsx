0ace4c1dde69741c34e441ab55aaf2dfff079a03
yogeshbangar <yogeshbangar@gmail.com>
2022-10-29 12:22:42 +0530

    resolve lint issue

--- components/util/index.tsx
+++ components/util/index.tsx
@@ -1,12 +1,12 @@
 import * as THREE from "three";
-export const setWireFrameMaterial = (model) => {
+export const setWireFrameMaterial = (model: any) => {
   if (!model) return;
   var material = new THREE.MeshNormalMaterial({
     wireframe: true,
     opacity: 0.02,
     transparent: true,
   });
-  model.traverse((object) => {
+  model.traverse((object: any) => {
     if (!object["isMesh"]) return;
     if (object["material"].isMaterial) {
       object["material"] = material;
