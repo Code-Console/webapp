156238f81c856113bdc13376f03ebdef9d81f690
yogeshbangar <yogeshbangar@gmail.com>
2022-11-18 16:52:47 +0530

    resolve lint

--- components/Fiber/Animtion/TextAnim/index.tsx
+++ components/Fiber/Animtion/TextAnim/index.tsx
@@ -12,7 +12,7 @@ const TextAnim = (props: any) => {
     new THREE.MeshBasicMaterial({ color: "#000000" })
   );
   mesh.position.set(0,0,-200);
-  let check: any[];
+  const check = new THREE.BufferGeometry();
   let geometry: THREE.ShapeGeometry | undefined;
   loader.load(`${basePath}3D/Lobster_Regular.json`, (font) => {
     const shapes = font?.generateShapes("This is for", 30);
@@ -24,7 +24,7 @@ const TextAnim = (props: any) => {
       new THREE.MeshBasicMaterial({ color: "#ff0000" })
     );
     mesh.add(testMesh);
-    check = [...geometry.attributes.position.array];
+    check.copy(geometry);
   });
 
   let count = 0;
@@ -36,15 +36,15 @@ const TextAnim = (props: any) => {
       geometry && check && i < geometry.attributes.position.array.length;
       i += 3
     ) {
-      if (count % geometry.attributes.position.array.length < i) {
-        geometry.attributes.position.array[i + 0] = check[i + 0] - 10;
-        geometry.attributes.position.array[i + 1] = check[i + 1] - 300;
-        geometry.attributes.position.array[i + 2] = check[i + 2] - 100;
-      } else {
-        geometry.attributes.position.array[i + 0] = check[i + 0];
-        geometry.attributes.position.array[i + 1] = check[i + 1];
-        geometry.attributes.position.array[i + 2] = check[i + 2];
-      }
+      // if (count % geometry.attributes.position.array.length < i) {
+      //   geometry.attributes.position.array[i + 0] = check[i + 0] - 10;
+      //   geometry.attributes.position.array[i + 1] = check[i + 1] - 300;
+      //   geometry.attributes.position.array[i + 2] = check[i + 2] - 100;
+      // } else {
+      //   geometry.attributes.position.array[i + 0] = check[i + 0];
+      //   geometry.attributes.position.array[i + 1] = check[i + 1];
+      //   geometry.attributes.position.array[i + 2] = check[i + 2];
+      // }
 
       geometry.attributes.position.needsUpdate = true;
     }
