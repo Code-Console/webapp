3c8fd459984b2f55e099599ac92981afcbc0b817
yogeshbangar <yogeshbangar@gmail.com>
2022-12-13 16:53:36 +0530

    add google analitics

--- components/UI/About.tsx
+++ components/UI/About.tsx
@@ -19,8 +19,8 @@ const About = ({ onClose, title }: { onClose: () => void; title: string }) => {
                   <div>
                     <span>
                       HI, I AM Yogesh Bangar. I AM A FULL STACK WEB DEVELOPER
-                      BASED IN ESSAOUIRA, MOROCCO. THIS WEBSITE IS A PERSONAL
-                      PROJECT THAT SERVES AS A PORTFOLIO. BUILT USING REACT,
+                      BASED IN INDORE, INDIA. THIS WEBSITE IS A PERSONAL PROJECT
+                      THAT SERVES AS A PORTFOLIO. BUILT USING REACT,
                       THREE.JS/REACT-THREE-FIBER AND BLENDER. FEEL FREE TO
                       CONTACT ME USING THE CONTACT PAGE OR THROUGH ONE OF THE
                       SOCIAL MEDIA PROFILES.
--- components/UI/HTMLHeader.tsx
+++ components/UI/HTMLHeader.tsx
@@ -1,23 +1,44 @@
 import Head from "next/head";
 import React from "react";
 import { basePath, ogImg } from "../Assets";
+import TagManager from "react-gtm-module";
+import { useAnimType } from "../hooks";
 
 const getFavicon = () => `${basePath}images/favicon.ico`;
 const HTMLHeader = () => {
-  const title = "Web/App Developer";
+  const animationType = useAnimType();
+  const [state, setState] = React.useState({ title: "Web/App Developer" });
+  React.useEffect(() => {
+    const pathname = window?.location?.pathname.includes("/animation");
+
+    const tagManagerArgs = {
+      gtmId: "G-Z41JWLH95L",
+      events: {
+        animationType: pathname ? animationType : "Web/App Developer",
+      },
+    };
+    TagManager.initialize(tagManagerArgs);
+    setState((state) => {
+      return {
+        ...state,
+        title: pathname ? animationType : "Web/App Developer",
+      };
+    });
+  }, [animationType]);
+
   const description =
     "Excellent knowledge of ThreeJS, ReactJS, NodeJS, PhaserJS, and HTML5, including experience with scripting, textures, animation, GUI styles, and particle systems Experience with Application development in Web and App Strong understanding of object-oriented programming Experience";
   return (
     <Head>
-      <title>{title}</title>
+      <title>{state.title}</title>
       <meta name="description" content={description} />
-      <meta property="og:title" content={title}></meta>
+      <meta property="og:title" content={state.title}></meta>
       <meta property="og:description" content={description}></meta>
       <meta property="og:image" content={ogImg}></meta>
       <meta property="og:site_name" content={"yogesh bangar"}></meta>
       <meta name="viewport" content={`width=device-width,initial-scale=1`} />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={state.title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={ogImg} />
       <meta property="twitter:site" content={"yogesh-bangar"}></meta>
