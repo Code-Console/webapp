c2c133be6c7e6fa65daaee789a6dd041a76ec82e
yogeshbangar <yogeshbangar@gmail.com>
2023-01-04 15:57:27 +0530

    add state for meeting

--- components/meeting/index.tsx
+++ components/meeting/index.tsx
@@ -1,5 +1,17 @@
+import { drop, first } from "lodash";
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  DevicePermissionStatus,
+  IDevicePermission,
+  JitsiConnectionStatus,
+  JitsiParticipant,
+  ParticipantMeetingState,
+} from "../../interfaces/meeting";
+import { actionDidAddLocalTrack } from "../../redux/action/meeting";
+import { logEvent, logJitsiEvent } from "../analytics";
 import { jitsiConfig } from "./config";
+import tryConnect from "./connection";
 import { MeetingContext } from "./MeetingContext";
 
 const Meeting = () => {
@@ -136,7 +148,7 @@ const Meeting = () => {
   function onConnectionSuccess() {
     console.error("~~~a~~", conference);
     room = connection.initJitsiConference(conference, confOptions);
-    setRoom(room)
+    setRoom(room);
     room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
     room.on(JitsiMeetJS.events.conference.TRACK_REMOVED, (track: any) => {
       delete remoteTracks[track.getParticipantId()];
@@ -319,9 +331,451 @@ const Meeting = () => {
     }
   };
 
+  /******************************************************* */
+  const dispatch = useDispatch();
+  const [permissionStatus, setPermissionStatus] =
+    React.useState<IDevicePermission>({
+      video: DevicePermissionStatus.WAITING,
+      audio: DevicePermissionStatus.WAITING,
+    });
+
+  const recoverFromFailedCreateLocalTracks = (
+    facingMode = "user",
+    constraints = undefined
+  ) => {
+    let videoDeviceIds: string[] = [];
+
+    const tryWithVideoDevice = () => {
+      if (videoDeviceIds.length === 0) {
+        return JitsiMeetJS.createLocalTracks({
+          devices: ["audio"],
+          facingMode,
+          constraints,
+        });
+      }
+      const cameraDeviceId = first(videoDeviceIds);
+      videoDeviceIds = drop(videoDeviceIds);
+      return JitsiMeetJS.createLocalTracks({
+        devices: ["audio", "video"],
+        cameraDeviceId,
+        facingMode,
+        constraints,
+      }).catch(tryWithVideoDevice);
+    };
+
+    return navigator.mediaDevices
+      .enumerateDevices()
+      .then((devices) => {
+        videoDeviceIds = devices
+          .filter((d) => d.kind === "videoinput")
+          .map((d) => d.deviceId);
+      })
+      .then(() => tryWithVideoDevice());
+  };
+
+  const createLocalTracks = async (
+    facingMode = "user",
+    constraints = undefined
+  ) => {
+    return JitsiMeetJS.createLocalTracks({
+      devices: ["audio", "video"],
+      facingMode,
+      constraints,
+    })
+      .catch((e: any) => {
+        console.error(e);
+        return recoverFromFailedCreateLocalTracks(facingMode, constraints);
+      })
+      .then((tracks: any) => {
+        // for (let index = 0; index < tracks.length; index++) {
+        //   const track = tracks[index];
+        //   if (track.getType() === 'audio') {
+        //     dispatch(actionSetAudioInput(track.getDeviceId()));
+        //   }
+        //   if (track.getType() === 'video') {
+        //     dispatch(actionSetVideoInput(track.getDeviceId()));
+        //   }
+        // }
+        // meetingDebugLogger(DID_CREATE_LOCAL_TRACKS, {
+        //   video: tracks.some((t) => t.getType() === 'video'),
+        //   audio: tracks.some((t) => t.getType() === 'audio')
+        // });
+        dispatch(actionDidAddLocalTrack(tracks));
+      });
+  };
+
+  // const transport = getConnectionTransport();
+  // const jitsiRegion = getJitsiRegionByRegionAndStore(region, storeId);
+  // const jitsiConfig = jitsiRegion
+  //   ? getConfigByRegion(meetingId, transport, jitsiRegion)
+  //   : getInitConfig(meetingId, transport, storeId);
+  const initMeeting = () => {
+    try {
+      console.log("jitsiConfig~~~~~~~~~~~~", jitsiConfig);
+      JitsiMeetJS.init(jitsiConfig);
+      JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.ERROR);
+      // meetingDebugLogger(DID_INIT_JITSI);
+      const facingMode = "user";
+      const constraints = undefined;
+      createLocalTracks(facingMode, constraints)
+        .catch((e) => {
+          console.error(`createLocalTracks failed with error`, e);
+          // Sentry.captureException(e);
+          // meetingDebugLogger(DID_FAIL_TO_CREATE_LOCAL_TRACKS, { error: e });
+          return createLocalTracks();
+        })
+        .then(() => {
+          // initDeviceList();
+          return tryConnect(jitsiConfig);
+        })
+        .then((connection) => {
+          // meetingDebugLogger(DID_CONNECT_TO_SERVER);
+          setConnection(connection);
+          // dispatch(actionUpdateMeetingServerUrl(jitsiConfig.hosts?.domain));
+        })
+        .catch(() => {
+          //TODO: Provide a way to handle when connect failed
+          // meetingDebugLogger(DID_FAIL_TO_CONNECT_TO_SERVER);
+          console.error("Connect to server failed");
+        });
+    } catch (e) {
+      // meetingDebugLogger(DID_FAIL_TO_INIT_JITSI, { error: e });
+      console.log("error invoking JitsiMeetJS init");
+      console.log(e);
+    }
+  };
+
+  const getUserMedia = (): Promise<IDevicePermission> => {
+    if (navigator?.mediaDevices?.getUserMedia) {
+      return navigator.mediaDevices
+        .getUserMedia({ audio: true, video: true })
+        .then(() => ({
+          audio: DevicePermissionStatus.GRANTED,
+          video: DevicePermissionStatus.GRANTED,
+        }))
+        .catch(() => {
+          //Assuming that can't get video permission, retry with audio only
+          console.log("DEBUG:: Can not get camera permission");
+          return navigator.mediaDevices
+            .getUserMedia({
+              audio: true,
+              video: false,
+            })
+            .then(() => ({
+              audio: DevicePermissionStatus.GRANTED,
+              video: DevicePermissionStatus.REJECTED,
+            }));
+        });
+    }
+    return Promise.reject(
+      new Error("navigator.mediaDevices.getUserMedia is not available")
+    );
+  };
+
+  const joinMeeting = (meetingId: string) => {
+    getUserMedia()
+      .then((permission) => {
+        setPermissionStatus(permission);
+        // dispatch(actionSetMeetingId(meetingId));
+        initMeeting();
+        // logEvent(DID_SET_DEVICE_PERMISSION, DID_SET_DEVICE_PERMISSION, {
+        //   audio: permission.audio,
+        //   video: permission.video,
+        //   meetingId
+        // });
+        // meetingDebugLogger(DID_GET_USER_MEDIA, permission);
+      })
+      .catch((error) => {
+        setPermissionStatus({
+          audio: DevicePermissionStatus.REJECTED,
+          video: DevicePermissionStatus.REJECTED,
+        });
+        // logEvent(DID_SET_DEVICE_PERMISSION, DID_SET_DEVICE_PERMISSION, {
+        //   audio: DevicePermissionStatus.REJECTED,
+        //   video: DevicePermissionStatus.REJECTED,
+        //   meetingId
+        // });
+        // meetingDebugLogger(DID_FAIL_TO_GET_USER_MEDIA, { error });
+      });
+  };
+  const joinRoom = async () => {
+    if (!room) {
+      const room = connection.initJitsiConference(conference, jitsiConfig);
+      room.setLocalParticipantProperty(
+        "meetingState",
+        ParticipantMeetingState.IN_THE_LOUNGE
+      );
+      room.join();
+      window["room" as any] = room;
+      setRoom(room);
+    }
+  };
+
+
+
+
+
+
+
+  const onTrackAdded = (track:any) => {
+    console.log(
+      `onTrackAdded local -  isLocal: ${track.isLocal()}, type - ${track.getType()}, id - ${track.getId()}`
+    );
+    if (track.isLocal()) {
+      return;
+    }
+    // dispatch(actionDidAddRemoteTrack(track));
+    // const action =
+    //   track.getType() === 'audio'
+    //     ? actionRemoteTrackAudioMuteDidChange
+    //     : actionRemoteTrackVideoMuteDidChange;
+    // dispatch(action(track.getParticipantId(), track.isMuted()));
+  };
+
+  const onTrackRemoved = (track:any) => {
+    console.log(
+      `track removed!!! ${track} id - ${track.getId()} local?: ${track.isLocal()}`
+    );
+    // if (track.isLocal()) {
+    //   dispatch(actionDidRemoveLocalTrack(track));
+    // } else {
+    //   dispatch(actionDidRemoveRemoteTrack(track));
+    // }
+  };
+  const onConferenceJoinedA = () => {
+    console.log('conference joined!');
+    // dispatch(actionDidJoinMeeting(room?.myUserId(), meetingRole));
+    // logEvent(DID_JOIN_MEETING, DID_JOIN_MEETING, {
+    //   participantId: room?.myUserId(),
+    //   meetingId,
+    //   role: meetingRole
+    // });
+  };
+
+  const onUserJoined = (participantId:any, user:any) => {
+    console.log(
+      `user join - ${participantId}, displayName - ${user.getDisplayName()}`
+    );
+    // dispatch(
+    //   actionUpdateRemoteDisplayName(participantId, user.getDisplayName())
+    // );
+    // dispatch(actionParticipantDidJoinMeeting(participantId, user));
+  };
+
+  const onUserPropertyChanged = (user:any) => {
+    console.log('user property changed', user?.getProperty('meetingState'));
+    const remoteMeetingState = user.getProperty('meetingState');
+    if (remoteMeetingState === ParticipantMeetingState.ENTERED_FROM_LOUNGE) {
+      // dispatch(actionParticipantDidEnterFromLounge(user.getId()));
+    }
+  };
+  const onUserLeftA = (id:any) => {
+    console.log(id + ' user left');
+    // dispatch(actionParticipantDidLeaveMeeting(id));
+  };
+
+  const onDisplayNameChanged = (participantId:any, displayName:any) => {
+    console.log(`onDisplayNameChanged: ${participantId} - ${displayName}`);
+    // if (participantId !== room?.myUserId()) {
+    //   dispatch(actionUpdateRemoteDisplayName(participantId, displayName));
+    // }
+  };
+
+  const onEndpointMessage = (_:any, payload:any) => {
+    // dispatch(actionDidReceiveEndpointMessage(payload));
+  };
+
+  const onTrackMuteChanged = (track:any) => {
+    if (track.isLocal()) {
+      return;
+    }
+    console.log(
+      `${track.getType()} track mute changed to muted ${track.isMuted()}, for participant ${track.getParticipantId()}`
+    );
+    // const action =
+    //   track.getType() === 'video'
+    //     ? actionRemoteTrackVideoMuteDidChange(
+    //         track.getParticipantId(),
+    //         track.isMuted()
+    //       )
+    //     : actionRemoteTrackAudioMuteDidChange(
+    //         track.getParticipantId(),
+    //         track.isMuted()
+    //       );
+    // dispatch(action);
+  };
+
+  const onRemoteStatsUpdated = (participantId: string, stats: any) => {
+    console.log(
+      `onRemoteStatsUpdated for participantId ${participantId}`,
+      stats
+    );
+    // dispatch(actionUpdateRemoteUserMeetingStats(participantId, stats));
+  };
+
+  const onLocalStatsUpdated = (stats: any) => {
+    console.log(`onLocalStatsUpdated`, stats);
+    if (stats?.connectionQuality < 10) {
+      logJitsiEvent('LOW_CONNECTION_QUALITY', {
+        conference,
+        participantId: room?.myUserId()
+      });
+    }
+    // dispatch(actionUpdateMeetingStats(stats));
+  };
+
+
+  const onDominantSpeakerChanged = (id: string) => {
+    // if (enteredMeetingFromLounge) {
+    //   dispatch(actionDidChangeDominantSpeaker(id));
+    // }
+  };
+
+  const onParticipantStatusChanged = (
+    participantId: string,
+    status: JitsiConnectionStatus
+  ) => {
+    // dispatch(actionDidChangeConnectionStatus({ participantId, status }));
+    logEvent(
+      'DID_CHANGE_PARTICIPANT_CONN_STATUS',
+      'DID_CHANGE_PARTICIPANT_CONN_STATUS',
+      { participantId, status }
+    );
+  };
+
+  const onLastNEndpointChange = (
+    leavingEndpointIds: string[],
+    enteringEndpointIds: string[]
+  ) =>
+    logJitsiEvent('LAST_N_ENDPOINTS_CHANGED', {
+      leavingEndpointIds,
+      enteringEndpointIds
+    });
+  const onSubjectChange = (subject: string) =>
+    logJitsiEvent('SUBJECT_CHANGED', { subject });
+  const onConferenceLeft = () => logJitsiEvent('CONFERENCE_LEFT');
+  const onConferenceUniqueIdSet = (meetingId: string) =>
+    logJitsiEvent('CONFERENCE_UNIQUE_ID_SET', { meetingId });
+  const onDMTFSupportChanged = (support: boolean) =>
+    logJitsiEvent('DTMF_SUPPORT_CHANGED', { support });
+  const onUserRoleChanged = (id: string, role: string) =>
+    logJitsiEvent('USER_ROLE_CHANGED', { id, role });
+  const onUserStatusChanged = (id: string, status: string) =>
+    logJitsiEvent('USER_STATUS_CHANGED', { id, status });
+  const onConferenceFailed = (errorCode: string) =>
+    logJitsiEvent('CONFERENCE_FAILED', { errorCode });
+  const onConferenceError = (errorCode: string) =>
+    logJitsiEvent('CONFERENCE_ERROR', { errorCode });
+  const onUserKicked = (actorParticipant: JitsiParticipant, reason: string) =>
+    logJitsiEvent('KICKED', { actorParticipant, reason });
+  const onParticipantKicked = (
+    actorParticipant: JitsiParticipant,
+    kickedParticipant: JitsiParticipant,
+    reason: string
+  ) =>
+    logJitsiEvent('PARTICIPANT_KICKED', {
+      actorParticipant,
+      kickedParticipant,
+      reason
+    });
+  const onStartMutedPolicyChanged = (policy: {
+    audio: boolean;
+    video: boolean;
+  }) => logJitsiEvent('START_MUTED_POLICY_CHANGED', policy);
+  const onStartedMuted = () => logJitsiEvent('STARTED_MUTED');
+  const onNoAudioInput = () => logJitsiEvent('NO_AUDIO_INPUT');
+  const onAudioInputStateChange = () =>
+    logJitsiEvent('AUDIO_INPUT_STATE_CHANGE');
+  const onRestarted = () => logJitsiEvent('CONFERENCE_RESTARTED');
+  const onVideoBridgeNotAvailable = () =>
+    logJitsiEvent('VIDEOBRIDGE_NOT_AVAILABLE');
+  const onReservationError = () => logJitsiEvent('RESERVATION_ERROR', null);
+  const onGracefulShutdown = () => logJitsiEvent('GRACEFUL_SHUTDOWN', null);
+  const onJingleFatalError = (error) =>
+    logJitsiEvent('INCOMPATIBLE_SERVER_VERSIONS', { error });
+  const onConferenceDistroyed = () => logJitsiEvent('CONFERENCE_DESTROYED', null);
+  const onConferenceMaxUser = () => logJitsiEvent('CONFERENCE_MAX_USERS', null);
+  const onDataChannelOpen = () => {
+    // dispatch(actionBridgeChannelOpened());
+  };
+  const roomListener = (status: 'on' | 'off') => {
+    const JitsiConferenceErrors = JitsiMeetJS.errors.conference;
+    const JitsiConferenceEvents = JitsiMeetJS.events.conference;
+    const JitsiConnectionQualityEvents = JitsiMeetJS.events.connectionQuality;
+    const listeners: {
+      [key: string]: Function;
+    } = {
+      [JitsiConferenceEvents.DATA_CHANNEL_OPENED]: onDataChannelOpen,
+      [JitsiConferenceEvents.PARTICIPANT_PROPERTY_CHANGED]:
+        onUserPropertyChanged,
+      [JitsiConferenceEvents.TRACK_REMOVED]: onTrackRemoved,
+      [JitsiConferenceEvents.TRACK_ADDED]: onTrackAdded,
+      [JitsiConferenceEvents.PARTICIPANT_CONN_STATUS_CHANGED]:
+        onParticipantStatusChanged,
+      [JitsiConferenceEvents.DOMINANT_SPEAKER_CHANGED]:
+        onDominantSpeakerChanged,
+      [JitsiConferenceEvents.CONFERENCE_JOINED]: onConferenceJoinedA,
+      [JitsiConferenceEvents.USER_JOINED]: onUserJoined,
+      [JitsiConferenceEvents.USER_LEFT]: onUserLeftA,
+      [JitsiConferenceEvents.DISPLAY_NAME_CHANGED]: onDisplayNameChanged,
+      [JitsiConferenceEvents.ENDPOINT_MESSAGE_RECEIVED]: onEndpointMessage,
+      [JitsiConferenceEvents.TRACK_MUTE_CHANGED]: onTrackMuteChanged,
+      [JitsiConnectionQualityEvents.REMOTE_STATS_UPDATED]: onRemoteStatsUpdated,
+      [JitsiConnectionQualityEvents.LOCAL_STATS_UPDATED]: onLocalStatsUpdated,
+      [JitsiConferenceEvents.LAST_N_ENDPOINTS_CHANGED]: onLastNEndpointChange,
+      [JitsiConferenceEvents.SUBJECT_CHANGED]: onSubjectChange,
+      [JitsiConferenceEvents.CONFERENCE_LEFT]: onConferenceLeft,
+      [JitsiConferenceEvents.CONFERENCE_UNIQUE_ID_SET]: onConferenceUniqueIdSet,
+      [JitsiConferenceEvents.DTMF_SUPPORT_CHANGED]: onDMTFSupportChanged,
+      [JitsiConferenceEvents.USER_ROLE_CHANGED]: onUserRoleChanged,
+      [JitsiConferenceEvents.USER_STATUS_CHANGED]: onUserStatusChanged,
+      [JitsiConferenceEvents.CONFERENCE_FAILED]: onConferenceFailed,
+      [JitsiConferenceEvents.KICKED]: onUserKicked,
+      [JitsiConferenceEvents.PARTICIPANT_KICKED]: onParticipantKicked,
+      [JitsiConferenceEvents.START_MUTED_POLICY_CHANGED]:
+        onStartMutedPolicyChanged,
+      [JitsiConferenceEvents.STARTED_MUTED]: onStartedMuted,
+      [JitsiConferenceEvents.NO_AUDIO_INPUT]: onNoAudioInput,
+      [JitsiConferenceEvents.AUDIO_INPUT_STATE_CHANGE]: onAudioInputStateChange,
+      [JitsiConferenceErrors.CONNECTION_ERROR]: onConferenceError,
+      [JitsiConferenceErrors.CONFERENCE_RESTARTED]: onRestarted,
+      [JitsiConferenceErrors.VIDEOBRIDGE_NOT_AVAILABLE]:
+        onVideoBridgeNotAvailable,
+      [JitsiConferenceErrors.RESERVATION_ERROR]: onReservationError,
+      [JitsiConferenceErrors.GRACEFUL_SHUTDOWN]: onGracefulShutdown,
+      [JitsiConferenceErrors.INCOMPATIBLE_SERVER_VERSIONS]: onJingleFatalError,
+      [JitsiConferenceErrors.CONFERENCE_DESTROYED]: onConferenceDistroyed,
+      [JitsiConferenceErrors.CONFERENCE_MAX_USERS]: onConferenceMaxUser
+    };
+    for (const event in listeners) {
+      room[status](event, listeners[event]);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+  React.useEffect(() => {
+    if (connection) joinRoom();
+  }, [connection]);
+  React.useEffect(() => {
+    if (!room ) return;
+    roomListener('on');
+    return () => {
+      room && roomListener('off');
+    };
+  }, [room]);
+  /****************************************************** */
+
   return (
     <div>
-      <button onClick={initJitsi}>initJitsi</button>
+      <button onClick={() => initJitsi()}>initJitsi</button>
     </div>
   );
 };
