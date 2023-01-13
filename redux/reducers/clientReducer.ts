8070830811d30aa07205919f81b5063b648135fb
yogeshbangar <yogeshbangar@gmail.com>
2023-01-13 17:29:21 +0530

    Add Lunge Screen.

--- redux/reducers/clientReducer.ts
+++ redux/reducers/clientReducer.ts
@@ -1,6 +1,5 @@
 import { AnyAction, Reducer } from "redux";
 import { ClientState } from "../../interfaces";
-import { DID_LOAD_FIBER, SET_ANIM_TYPE, SET_ORBIT_CONTROL } from "../action";
 import common3DReducer from "./common3DReducer";
 import meetingReducer from "./meetingReducer";
 export const defaultClientState: ClientState = {
@@ -22,34 +21,3 @@ const clientReducer = reduceReducers(defaultClientState, [
 ]);
 
 export default clientReducer;
-
-// const clientReducer = (
-//   state: ClientState = defaultClientState,
-//   action: AnyAction
-// ): ClientState => {
-//   switch (action.type) {
-//     case DID_LOAD_FIBER: {
-//       return {
-//         ...state,
-//         isAllModelLoaded: action.payload,
-//       };
-//     }
-//     case SET_ANIM_TYPE: {
-//       return {
-//         ...state,
-//         animType: action.payload,
-//       };
-//     }
-//     case SET_ORBIT_CONTROL: {
-//       console.log("action.payload", action.payload);
-//       return {
-//         ...state,
-//         isOrbitControl: action.payload,
-//       };
-//     }
-//     default:
-//       return state;
-//   }
-// };
-
-// export default clientReducer;
